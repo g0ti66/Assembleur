44e87c962e58da58c386f8b7c0201202fc69bed0
gauthy66 <gauthy.dussart66@gmail.com>
2025-12-20 18:48:14 +0100

    First Commit

---
+++ 1_hello/hello.asm
@@ -0,0 +1,17 @@
+section .data
+    msg db "Hello x86-64", 10
+    len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1      ; write
+    mov rdi, 1      ; stdout
+    mov rsi, msg
+    mov rdx, len
+    syscall
+
+    mov rax, 60     ; exit
+    xor rdi, rdi
+    syscall
